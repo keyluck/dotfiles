d1d6498885e40cf7ed6f1cf497b3a3d9d02b4c10
Nick Luckey <nickluckey@gmail.com>
2022-07-07 12:55:30 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,15 @@ zshrc() {
     echo "                  Import zshrc                             "
     echo "-----------------------------------------------------------"
     cat .zshrc > $HOME/.zshrc
+    cp .bash_aliases ~/.bash_aliases
+    mkdir -p ~/.kube
+    echo "$KUBECONFIG_NONPROD_NEW" > ~/.kube/config
+
+    echo "$KUBECONFIG_PROD" > ~/.kube/prod-config
+    KUBECONFIG=~/.kube/config:~/.kube/prod-config kubectl config view --flatten > /tmp/config
+    mv /tmp/config ~/.kube/config
 }
 
 zshrc
 
-cp .bash_aliases ~/.bash_aliases
-mkdir ~/.kube
-echo "$KUBECONFIG_NONPROD_NEW" > ~/.kube/config
 
-echo "$KUBECONFIG_PROD" > ~/.kube/prod-config
-KUBECONFIG=~/.kube/config:~/.kube/prod-config kubectl config view --flatten > /tmp/config
-mv /tmp/config ~/.kube/config
